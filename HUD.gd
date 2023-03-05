5104fcefe64c15108ba28648b2c771766edd3543
Stephen J. Lynch <citybadger@gmail.com>
2023-03-05 13:13:52 -0500

    Separate Start Menu from Main scene.

--- HUD.gd
+++ HUD.gd
@@ -11,19 +11,11 @@ func show_game_over():
 	show_message("Game Over")
 	# Wait until the MessageTImer had counted down.
 	await $MessageTimer.timeout
+	get_tree().change_scene_to_file("res://startMnu.tscn")
 	
-	$Message.text = "Et tu?"
-	$Message.show()
-	# Make a one-shot timer and wait for it to finish.
-	await get_tree().create_timer(1.0).timeout
-	$StartButton.show()
 
 func update_score(score):
 	$ScoreLabel.text = str(score)
-	
-func _on_Start_Button_pressed():
-	$StartButton.hide()
-	start_game.emit()
 	
 func _on_MessageTimer_timeout():
 	$Message.hide()
